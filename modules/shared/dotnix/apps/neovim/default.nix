82ed8be0d7d0176e73177435ab61f311defb1b0d
Hawtian Wang <twistoy.wang@gmail.com>
2026-03-30 17:49:18 +0800

    feat(neovim): add dotvim repo auto-clone activation script

--- modules/shared/dotnix/apps/neovim/default.nix
+++ modules/shared/dotnix/apps/neovim/default.nix
@@ -1,16 +1,30 @@
 {
   config,
   lib,
+  inputs,
   dotnix-utils,
+  dotnix-constants,
   ...
 }: let
   cfg = config.dotnix.apps.neovim;
+  inherit (dotnix-constants) user;
+  homeDir = config.users.users."${user.name}".home;
+  hm-dag = inputs.home-manager.lib.hm.dag;
 in {
   options.dotnix.apps.neovim = {
     enable = lib.mkEnableOption "Enable module dotnix.apps.neovim";
   };
 
   config = lib.mkIf cfg.enable {
-    home-manager = dotnix-utils.hm.hmModule ./home.nix;
+    home-manager = lib.mkMerge [
+      (dotnix-utils.hm.hmModule ./home.nix)
+      (dotnix-utils.hm.hmConfig {
+        home.activation.setup-dotvim = hm-dag.entryAfter ["linkGeneration"] ''
+          if [ ! -d "${homeDir}/.dotvim" ]; then
+            $DRY_RUN_CMD git clone git@github.com:TwIStOy/dotvim.git "${homeDir}/.dotvim"
+          fi
+        '';
+      })
+    ];
   };
 }
