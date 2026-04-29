76cd31cd34c0f44e157fba7ca2ed0581b5e4a7c3
Hawtian Wang <twistoy.wang@gmail.com>
2026-04-29 10:55:56 +0800

    fix: ssl error

--- modules/darwin/core.nix
+++ modules/darwin/core.nix
@@ -4,18 +4,23 @@
   ...
 }: let
   inherit (dotnix-constants) user;
+
+  caBundle = pkgs.runCommand "ca-bundle-zscaler" {} ''
+    cat ${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt ${./zscaler-root-ca.pem} > $out
+  '';
 in {
   nix = {
-    enable = false;
+    enable = true;
 
     package = pkgs.nix;
 
-    # Disable auto-optimise-store because of this issue:
-    #   https://github.com/NixOS/nix/issues/7273
-    # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists"
     settings.auto-optimise-store = false;
 
     gc.automatic = false;
+
+    extraOptions = ''
+      ssl-cert-file = ${caBundle}
+    '';
   };
 
   nixpkgs.config.allowUnfree = true;
