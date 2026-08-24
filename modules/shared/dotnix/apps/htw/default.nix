15551ad0095b9fd63b0d7bdad8aa19ac865281c2
Hawtian Wang <twistoy.wang@gmail.com>
2026-08-24 18:16:20 +0800

    feat(dotnix): add htw fish shell completion and integration
    Set up fish completions and shell integration for the htw CLI when the fish module is enabled, enhancing the developer experience by providing command completion and shell hooks.

--- modules/shared/dotnix/apps/htw/default.nix
+++ modules/shared/dotnix/apps/htw/default.nix
@@ -3,10 +3,20 @@
   lib,
   pkgs,
   inputs,
+  dotnix-utils,
   ...
 }: let
   cfg = config.dotnix.apps.htw;
   htw = inputs.htw.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  htwBin = lib.getExe cfg.package;
+  fishCompletion = pkgs.runCommandLocal "htw-fish-completion" {} ''
+    ${htwBin} completion fish >"$out"
+  '';
+  fishIntegration = pkgs.runCommandLocal "htw-fish-integration" {} ''
+    export XDG_CONFIG_HOME="$TMPDIR/xdg"
+    ${htwBin} shell install >/dev/null
+    cp "$XDG_CONFIG_HOME/fish/functions/htw.fish" "$out"
+  '';
 in {
   options.dotnix.apps.htw = {
     enable = lib.mkEnableOption "htw, the developer environment manager CLI";
@@ -23,5 +33,12 @@ in {
     dotnix.hm.packages = [
       cfg.package
     ];
+
+    home-manager = lib.mkIf config.dotnix.apps.fish.enable (dotnix-utils.hm.hmConfig {
+      xdg.configFile = {
+        "fish/completions/htw.fish".source = fishCompletion;
+        "fish/functions/htw.fish".source = fishIntegration;
+      };
+    });
   };
 }
