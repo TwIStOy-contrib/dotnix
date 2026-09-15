b0a1dd04f83eefb96a6b39e656b9e50b32dcb27c
Hawtian Wang <twistoy.wang@gmail.com>
2026-09-15 12:18:31 +0800

    feat: add git config
    Signed-off-by: Hawtian Wang <twistoy.wang@gmail.com>

--- hosts/taihou/home.nix
+++ hosts/taihou/home.nix
@@ -1,4 +1,12 @@
 {pkgs, ...}: {
+  programs.git = {
+    # Route git http(s) traffic through the local mihomo mixed port.
+    settings = {
+      http.proxy = "http://127.0.0.1:7893";
+      https.proxy = "http://127.0.0.1:7893";
+    };
+  };
+
   programs.ssh = {
     # Equivalent to the old `Host github.com / ProxyCommand` extraConfig block:
     # route github ssh through the local mihomo mixed port.
